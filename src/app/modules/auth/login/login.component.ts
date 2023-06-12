9399ff24891f43e60e36d37f53a91f33fd6ce91c
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2023-06-12 18:32:43 +0545

    [REMOVED]: removed 'withCredentials: true' options from login post request

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -24,7 +24,7 @@ export class LoginComponent implements OnInit {
   }
 
   submit(){
-    this.http.post('http://localhost:3000/api/login', this.loginForm.getRawValue(), {withCredentials: true})
+    this.http.post('http://localhost:3000/api/login', this.loginForm.getRawValue())
     .subscribe({
       next: (res: any) => {
         AuthInterceptor.accessToken = res.access_token;
